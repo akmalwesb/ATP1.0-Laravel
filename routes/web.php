3962cc8442a477e4ac6216369e393a7e33bd45b3
MuhdNurHusaini <husainirudin00@icloud.com>
2023-04-06 12:37:09 +0800

    modified web.php

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use App\Http\Controllers\laravel_example\UserManagement;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-test github
 |
 */
 
