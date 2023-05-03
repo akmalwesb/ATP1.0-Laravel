28f207489dac6150c59039189e653e2c9f376ca6
fatinsorfina <fatin@weststar-engineering.com>
2023-05-03 14:59:44 +0800

    database update

--- config/database.php
+++ config/database.php
@@ -67,7 +67,7 @@ return [
             //'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'ATP'),
+            'database' => env('DB_DATABASE', 'weststar_database'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
@@ -82,12 +82,12 @@ return [
             ]) : [],
         ],
 
-        'mysqlATP' => [
+        'mysql' => [
             'driver' => 'mysql',
             // 'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'atp'),
+            'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
